8f7591b40a562baa8a04d636dfbbf321f8cc19b9
Sam Mulcahy <mulch@Sams-MacBook-Pro.local>
2023-02-16 17:35:58 -0600

    added local storage save functionality to all save buttons

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
-
-
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -12,9 +9,15 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  document.querySelectorAll('.saveBtn').on('click', function(){
-    
-  })
+  var saveBtns = document.querySelectorAll(".saveBtn");
+
+  $(saveBtns).each(function(){
+    $(this).on('click', function(){
+      localStorage.setItem($(this).parent().attr('id'), 
+      ($(this).siblings("textarea").val())
+      )
+    })
+  });
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
